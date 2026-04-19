6fb7affcea2852e674925952b21e3c4641aebb77
Evan Blanton <ejdb040607@hotmail.com>
2026-04-19 17:08:08 -0400

    Added abilitiy to fire ammo and having a limit to the amount. Added laser sound.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
 
 class AlienInvasion:
 
@@ -23,7 +24,11 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
+        self.laser_sound.set_volume(0.7)
+
+        self.ship = Ship(self, Arsenal(self))
 
     def run_game(self):
         # Game loop
@@ -54,6 +59,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                self.laser_sound.play()
+                self.laser_sound.fadeout(250)
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
--- ship.py
+++ ship.py
@@ -3,10 +3,11 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
 
 
 class Ship:
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -19,14 +20,18 @@ class Ship:
         
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.boundaries.midbottom
-
+        self.x = float(self.rect.x)
         self.moving_right = False
         self.moving_left = False
 
-        self.x = float(self.rect.x)
+        self.arsenal = arsenal
 
     def update(self):
         # Updating the position of the the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
         speed = self.settings.ship_speed
         
         if self.moving_right and self.rect.right < self.boundaries.right:
@@ -37,4 +42,8 @@ class Ship:
         self.rect.x = self.x
 
     def draw(self) -> None:
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        return self.arsenal.fire_bullet()
