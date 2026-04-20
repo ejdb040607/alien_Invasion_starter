f8396c14d48586d733362e708ebad6932feac86e
Evan Blanton <ejdb040607@hotmail.com>
2026-04-20 12:22:10 -0400

    P2- Created an alien and displayed it on the screen. No movement.

--- settings.py
+++ settings.py
@@ -2,20 +2,31 @@ from pathlib import Path
 class Settings:
 
     def __init__(self):
+        # Game
         self.name: str = "Alien Invasion"
         self.screen_w = 900
         self.screen_h = 600
         self.fps = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
 
+        # Ship
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
 
+        # Bullet
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
+
+        # Alien
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+
+        # Fleet
+        self.fleet_speed = 5
