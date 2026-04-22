1a6667f8f732cff5849e933702732d214a37ec10
Evan Blanton <ejdb040607@hotmail.com>
2026-04-22 13:10:00 -0400

    Refactored calc offsets and fleet shape.

--- alien_fleet.py
+++ alien_fleet.py
@@ -23,14 +23,12 @@ class AlienFleet:
         screen_h = self.settings.screen_h
 
         fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
-        half_screen = self.settings.screen_h // 2
+        
+        x_offset, y_offset = self.calculate_offsets(alien_w, screen_w, alien_h, fleet_w, fleet_h)
 
-        fleet_horizontal_space = fleet_w * alien_w
-        x_offset = int((screen_w - fleet_horizontal_space) // 2)
+        self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
 
-        fleet_vertical_space = fleet_h * alien_h
-        y_offset = int((half_screen - fleet_vertical_space) // 2)
-
+    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
         for row in range(fleet_h):
             current_y = alien_h * row + y_offset
             if row % 2 == 0:
@@ -40,6 +38,17 @@ class AlienFleet:
                 if col % 2 == 0:
                     continue
                 self._create_alien(current_x, current_y)
+
+    def calculate_offsets(self, alien_w, screen_w, alien_h, fleet_w, fleet_h):
+        half_screen = self.settings.screen_h // 2
+
+        fleet_horizontal_space = fleet_w * alien_w
+        x_offset = int((screen_w - fleet_horizontal_space) // 2)
+
+        fleet_vertical_space = fleet_h * alien_h
+        y_offset = int((half_screen - fleet_vertical_space) // 2)
+
+        return x_offset, y_offset
 
 
     def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
